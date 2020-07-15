ea3164655b17bc6e1e2aaa2b38adba8bf5b339b2
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-07-14 19:25:04 -0700

    commit

--- msg_index_bot.py
+++ msg_index_bot.py
@@ -16,7 +16,7 @@ from db import db
 from search import searchText, searchChannel
 import sys
 from fetchIndex import backfillChannel
-from newdb import index, maintext, channelname, channelrefer, timestamp, channels, blocklist
+from newdb import index, maintext, channelrefer, timestamp, channels, blocklist
 
 HELP_MESSAGE = '''
 添加频道，群组 - "/add @dushufenxiang", 可批量。
