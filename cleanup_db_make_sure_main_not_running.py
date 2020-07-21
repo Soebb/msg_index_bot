44686d247b13361604da5745b394e4041e604586
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-07-21 04:57:47 -0700

    commit

--- cleanup_db_make_sure_main_not_running.py
+++ cleanup_db_make_sure_main_not_running.py
@@ -1,8 +1,21 @@
 import plain_db
 from telegram_util import removeOldFiles
+import dbase
+from dbase import index, maintext, timestamp
+from common import log_call
 
-if __name__ == '__main__':
+def cleanup1():
+	...
+
+log_call()
+def cleanup2():
 	plain_db.cleanupLargeDB('index')
 	plain_db.cleanupLargeDB('maintext')
+	plain_db.cleanupLargeDB('timestamp')
+	plain_db.cleanupLargeDB('channelrefer')
+	plain_db.cleanupLargeDB('channels')
 	removeOldFiles('tmp', day = 0.5)
-	print('finish')
+
+if __name__ == '__main__':
+	cleanup1()
+	cleanup2()
