de4867122650083dc5b5badb1a07804d5536c577
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-08-02 07:13:25 -0700

    commit

--- msg_index_bot.py
+++ msg_index_bot.py
@@ -17,7 +17,7 @@ import time
 @log_call()
 def indexingImp():
 	for channel, score in channels.items():
-		if score < 0 or random.random() > 1.0 / (score * score + 1):
+		if score < 0 or random.random() > 1.0 / (score ** 2.5 + 1):
 			continue
 		if 'test' in sys.argv and random.random() > 0.1:
 			continue # better testing
