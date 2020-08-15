ae759cc8b18a84b87d3298ce50df0c299338998e
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-08-15 14:51:48 -0700

    commit

--- msg_index_bot.py
+++ msg_index_bot.py
@@ -49,7 +49,7 @@ def indexBackfill():
 def indexing():
 	if len(coreIndex) == 0:
 		dbase.fillCoreIndex()
-	if len(dbase.maintext.items()) > 2400000:
+	if True: # if len(dbase.maintext.items()) > 2500000:
 		clean.indexClean()
 	indexingImp() 
 	indexBackfill()
