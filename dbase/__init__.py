0ce35b9f641a020b0824c97d8e94d3b26b954385
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-08-19 15:54:05 -0700

    commit

--- dbase/__init__.py
+++ dbase/__init__.py
@@ -173,12 +173,12 @@ def isCore(key):
 	if not (0 <= channels.get(channel) <= 3):
 		return False
 	if 0 <= channels.get(channel) <=1:
-		return timestamp.get(key, 0) > time.time() - 365 * 60 * 60 * 24
+		return timestamp.get(key, 0) > time.time() - 180 * 60 * 60 * 24
 	if channel in suspect._db.items:
 		return False
 	if channels.get(channel) == 2:
-		return timestamp.get(key, 0) > time.time() - 7 * 60 * 60 * 24
-	return timestamp.get(key, 0) > time.time() - 3 * 60 * 60 * 24
+		return timestamp.get(key, 0) > time.time() - 6 * 60 * 60 * 24
+	return timestamp.get(key, 0) > time.time() - 2 * 60 * 60 * 24
 
 @log_on_fail(debug_group)
 @log_call()
