a40dabfa11c439ca405b8a7e7f864b75221a482a
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-09-07 09:28:41 -0700

    commit

--- dbase/__init__.py
+++ dbase/__init__.py
@@ -18,8 +18,8 @@ status = {}
 badByRefer = set()
 
 def setChannelBatch(targets, score):
-	targets = [piece.lower() for target in targets 
-		for piece in target.strip('/').split('/')]
+	targets = [piece.lower() for item in targets 
+		for piece in item.strip('/').split('/')]
 	updated = 0
 	for channel, _ in channels.items():
 		if channel.lower() in targets:
